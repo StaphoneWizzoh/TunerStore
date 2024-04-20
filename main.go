9e44f113d0ebe2e54b8a4ec93a194e80fcf47e28
Staphone <staphoneomondi@gmail.com>
2024-04-20 18:24:05 +0300

    Error fixes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	p2p "github.com/StaphoneWizzoh/TunerStore/peer2peer"
@@ -9,10 +10,18 @@ import (
 func main(){
 	tcpOpts := p2p.TCPTransportOpts{
 		ListenAddr: ":3000",
-		Decoder: p2p.GOBDecoder{},
+		Decoder: p2p.DefaultDecoder{},
 		HandshakeFunc: p2p.NOPHandshakeFunc,
 	}
+
 	tr := p2p.NewTCPTransport(tcpOpts)
+
+	go func ()  {
+		for{
+			msg := <-tr.Consume()
+			fmt.Printf("Message: %v\n", msg)
+		}
+	}()
 
 	if err := tr.ListenAndAccept(); err != nil{
 		log.Fatal(err)
