67a0d14ea82c88259df5ebeee5f7206f07940f81
Staphone <staphoneomondi@gmail.com>
2024-04-20 19:08:40 +0300

    Started storage implementation

---
+++ store.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type PathTransformFunc func (string) string
+
+type StoreOpts struct{
+	PathTransformFunc PathTransformFunc
+}
+
+type Store struct{
+	StoreOpts
+}
+
+var DefaultPathTransformFunc = func (key string) string {
+	return key
+}
+
+func NewStore(opts StoreOpts) *Store{
+	return &Store{
+		StoreOpts: opts,
+	}
+}
+
+
+
+func (s *Store) writeStream(key string, r io.Reader) error{
+	pathName := s.PathTransformFunc(key)
+
+	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+		return err
+	}
+
+	fileName := "somefilename"
+	pathAndFileName := pathName + "/" + fileName
+
+	f, err := os.Create(pathAndFileName)
+	if err != nil {
+		return err
+	}
+
+	n, err := io.Copy(f, r)
+	if err != nil{
+		return err
+	}
+
+	log.Printf("written (%d) bytes to disk: %s", n, pathAndFileName)
+
+	return nil
+}
