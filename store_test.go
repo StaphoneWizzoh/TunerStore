47f8c2b5961b95a1fb4c6b892eacd4501ee965b8
Staphone <staphoneomondi@gmail.com>
2024-04-22 19:50:14 +0300

    Implememnted the use of the PathKey struct

--- store_test.go
+++ store_test.go
@@ -8,12 +8,17 @@ import (
 
 func TestPathTransformFunc(t *testing.T){
 	key := "MyLooks"
-	pathName := CASPathTransformFunc(key)
+	pathKey := CASPathTransformFunc(key)
+	expectedOriginalKey := "bdbdcc7520e373094b7a93d34a7ddcce3221e025"
 	expectedPathName := "bdbdc/c7520/e3730/94b7a/93d34/a7ddc/ce322/1e025"
-	fmt.Println(pathName)
+	fmt.Println(pathKey.PathName)
 
-	if pathName != expectedPathName{
-		t.Error(t, "Have %s want %s", pathName, expectedPathName)
+	if pathKey.PathName != expectedPathName{
+		t.Error(t, "Have %s want %s", pathKey.PathName, expectedPathName)
+	}
+
+	if pathKey.Original != expectedOriginalKey{
+		t.Error(t, "Have %s want %s", pathKey.Original, expectedOriginalKey)
 	}
 }
 
