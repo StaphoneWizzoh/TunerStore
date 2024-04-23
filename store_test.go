219c832ebde2623939640988ab06919ca2742608
Staphone <staphoneomondi@gmail.com>
2024-04-23 19:55:16 +0300

    renamed variable

--- store_test.go
+++ store_test.go
@@ -10,7 +10,7 @@ import (
 func TestPathTransformFunc(t *testing.T){
 	key := "MyLooks"
 	pathKey := CASPathTransformFunc(key)
-	expectedOriginalKey := "bdbdcc7520e373094b7a93d34a7ddcce3221e025"
+	expectedFileName := "bdbdcc7520e373094b7a93d34a7ddcce3221e025"
 	expectedPathName := "bdbdc/c7520/e3730/94b7a/93d34/a7ddc/ce322/1e025"
 	fmt.Println(pathKey.PathName)
 
@@ -18,8 +18,8 @@ func TestPathTransformFunc(t *testing.T){
 		t.Error(t, "Have %s want %s", pathKey.PathName, expectedPathName)
 	}
 
-	if pathKey.FileName != expectedOriginalKey{
-		t.Error(t, "Have %s want %s", pathKey.FileName, expectedOriginalKey)
+	if pathKey.FileName != expectedFileName{
+		t.Error(t, "Have %s want %s", pathKey.FileName, expectedFileName)
 	}
 }
 
