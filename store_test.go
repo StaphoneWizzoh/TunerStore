53746b3ab6d45d11a2c7089cbe74d91c531bb3ae
Staphone <staphoneomondi@gmail.com>
2024-04-23 19:49:46 +0300

    removed individual delete test sinece its within the store test

--- store_test.go
+++ store_test.go
@@ -23,23 +23,6 @@ func TestPathTransformFunc(t *testing.T){
 	}
 }
 
-func TestStroreDeleteKey(t *testing.T){
-	opts := StoreOpts{
-		PathTransformFunc: CASPathTransformFunc,
-	}
-	s := NewStore(opts)
-	key := "myPicture"
-	data := []byte("Some jpg bytes")
-
-	if err := s.writeStream(key,bytes.NewReader(data)); err != nil {
-		t.Error(err)
-	}
-
-	if err := s.Delete(key); err != nil{
-		t.Error(err)
-	}
-}
-
 func TestStore(t *testing.T) {
 	s := newStore()
 	defer teardown(t, s)
@@ -66,7 +49,14 @@ func TestStore(t *testing.T) {
 			t.Errorf("want %s have %s", data, b)
 		}
 
-		s.Delete(key)
+		if err := s.Delete(key); err != nil {
+			t.Error(err)
+		}
+
+		if ok := s.Has(key); ok{
+			t.Errorf("expected to NOT have key %s", key)
+		}
+
 	}
 }
 
