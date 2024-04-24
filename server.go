4aa5efc157f14868040cbe5b503dd90c3a2c380d
Staphone <staphoneomondi@gmail.com>
2024-04-24 13:07:23 +0300

    created server file

---
+++ server.go
@@ -0,0 +1,36 @@
+package main
+
+import p2p "github.com/StaphoneWizzoh/TunerStore/peer2peer"
+
+type FileServerOpts struct{
+	ListenAddr string
+	StorageRoot string
+	PathTransformFunc PathTransformFunc
+	Transport p2p.Transport
+}
+
+type FileServer struct{
+	FileServerOpts
+
+	store *Store
+}
+
+func NewFileServer(opts FileServerOpts) *FileServer{
+	storeOpts := StoreOpts{
+		Root: opts.StorageRoot,
+		PathTransformFunc: opts.PathTransformFunc,
+	}
+
+	return &FileServer{
+		FileServerOpts: opts,
+		store: NewStore(storeOpts),
+	}
+}
+
+func (s *FileServerOpts) Start() error{
+	if err := s.Transport.ListenAndAccept(); err != nil {
+		return err
+	}
+
+	return nil
+}
