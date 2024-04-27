82ed1e700df60eba5e0f8924e989ecc78e421396
Staphone <staphoneomondi@gmail.com>
2024-04-27 18:47:19 +0300

    broadcasting logic

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
+	"io"
 	"log"
 	"sync"
 
@@ -36,6 +38,27 @@ func NewFileServer(opts FileServerOpts) *FileServer{
 		quitCh: make(chan struct{}),
 		peers: make(map[string]p2p.Peer),
 	}
+}
+
+type Payload struct{
+	Key string
+	Data []byte
+}
+
+func (s *FileServer) broadcast(p Payload) error{
+	peers := []io.Writer{}
+
+	for _, peer := range s.peers{
+		peers = append(peers, peer)
+	}
+
+	multiWriter := io.MultiWriter(peers...) 
+	
+	return gob.NewEncoder(multiWriter).Encode(p)
+}
+
+func (s *FileServer) StoreData(key string, r io.Reader) error{
+	return nil
 }
 
 func (s *FileServer) Stop(){
