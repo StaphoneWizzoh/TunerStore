301388423950bf97999965897fa4ffbf9d5112c8
Staphone <staphoneomondi@gmail.com>
2024-05-12 12:50:22 +0300

    refactored

--- store.go
+++ store.go
@@ -114,31 +114,6 @@ func (s *Store) Write(key string, r io.Reader) (int64, error){
 	return s.writeStream(key, r)
 }
 
-func (s *Store) Read(key string) (int64, io.Reader, error){
-	return s.readStream(key)
-}
-
-func (s *Store) readStream(key string)(int64, io.ReadCloser, error){
-	pathKey := s.PathTransformFunc(key) 
-	fullPathWithRoot := fmt.Sprintf("%s/%s",s.Root, pathKey.fullPath())
-
-	// fi, err := os.Stat(fullPathWithRoot)
-	// if err != nil {
-	// 	return 0, nil, err
-	// }
-
-	file, err := os.Open(fullPathWithRoot)
-	if err != nil {
-		return 0, nil, err
-	}
-
-	fi ,err := file.Stat()
-	if err != nil {
-		return 0, nil ,err
-	}
-	return fi.Size(), file, err
-}
-
 func (s *Store) WriteDecrypt(encKey []byte, key string, r io.Reader)(int64, error){
 	f, err := s.openFileForWriting(key)
 	if err != nil {
@@ -169,3 +144,30 @@ func (s *Store) writeStream(key string, r io.Reader) (int64,error){
 	}
 	return io.Copy(f, r)
 }
+
+func (s *Store) Read(key string) (int64, io.Reader, error){
+	return s.readStream(key)
+}
+
+func (s *Store) readStream(key string)(int64, io.ReadCloser, error){
+	pathKey := s.PathTransformFunc(key) 
+	fullPathWithRoot := fmt.Sprintf("%s/%s",s.Root, pathKey.fullPath())
+
+	// fi, err := os.Stat(fullPathWithRoot)
+	// if err != nil {
+	// 	return 0, nil, err
+	// }
+
+	file, err := os.Open(fullPathWithRoot)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	fi ,err := file.Stat()
+	if err != nil {
+		return 0, nil ,err
+	}
+	return fi.Size(), file, err
+}
+
+
