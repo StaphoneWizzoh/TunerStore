211186edf911ec7f81f2bbd8c652b37c8022474a
Staphone <staphoneomondi@gmail.com>
2024-05-12 11:23:05 +0300

    modified writeStream to incorporate encryption

--- store.go
+++ store.go
@@ -139,6 +139,27 @@ func (s *Store) readStream(key string)(int64, io.ReadCloser, error){
 	return fi.Size(), file, err
 }
 
+func (s *Store) WriteDecrypt(encKey []byte, key string, r io.Reader)(int64, error){
+	pathKey := s.PathTransformFunc(key)
+	pathNameWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.PathName)
+
+	if err := os.MkdirAll(pathNameWithRoot, os.ModePerm); err != nil {
+		return 0, err
+	}
+
+	fullPathWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.fullPath())
+
+	f, err := os.Create(fullPathWithRoot)
+	if err != nil {
+		return 0, err
+	}
+
+	n, err := copyDecrypt(encKey, r, f)
+	return int64(n), err
+}
+
+
+
 func (s *Store) writeStream(key string, r io.Reader) (int64,error){
 	pathKey := s.PathTransformFunc(key)
 	pathNameWithRoot := fmt.Sprintf("%s/%s", s.Root, pathKey.PathName)
@@ -154,9 +175,5 @@ func (s *Store) writeStream(key string, r io.Reader) (int64,error){
 		return 0, err
 	}
 
-	n, err := io.Copy(f, r)
-	if err != nil{
-		return 0, err
-	}
-	return n, nil
+	return io.Copy(f, r)
 }
